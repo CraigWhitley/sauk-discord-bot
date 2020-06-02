59ac6cdc83fbc3e44c13525e7426e5a794ccb8ad
CraigWhitley <kiadauk@gmail.com>
2020-06-02 16:59:49 +0100

    chore: removed some not needed comments.

--- app.py
+++ app.py
@@ -58,13 +58,4 @@ async def on_message(message: discord.Message):
     # executes the default help command with *args.
     await commands[command](message, *command_args)
 
-    # If removing all messages before 30 days ago:
-    # amount_of_days = 30
-
-    # remove_before = dt.timedelta(days=-amount_of_days)
-
-    # if message.content.startswith('!purge'):
-    #     async for message in message.channel.history(before=remove_before):
-    #         await message.delete()
-
 client.run('NzE2ODY4MDcwNDIwNzc0OTQ0.XtSBlA.Ezt3Fq-03szdCnXK3lLwbdlQAqc')
